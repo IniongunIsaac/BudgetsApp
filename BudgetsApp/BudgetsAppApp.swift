86b303c7b79687a66102a80cfc2e30d2f86d7163
Isaac Iniongun <iniongunisaac@gmail.com>
2023-04-24 10:23:50 +0100

    Initial Commit

---
+++ BudgetsApp/BudgetsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetsAppApp.swift
+//  BudgetsApp
+//
+//  Created by Isaac Iniongun on 24/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct BudgetsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
